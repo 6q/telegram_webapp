3d2ae86694b780a4fb0f127a08dd0f69ab0bdd61
searcheverythings <searcheverythings@gmail.com>
2016-12-19 15:17:56 +0530

    form attribute "data-emojiable" add

--- app/Services/Html/FormBuilder.php
+++ app/Services/Html/FormBuilder.php
@@ -48,6 +48,41 @@ class FormBuilder extends \Collective\Html\FormBuilder {
 		);
 	}
 	
+	public function control_attr($type, $colonnes, $nom, $errors, $label = null, $valeur = null, $attr = null ,$pop = null, $placeholder = '')
+	{
+		if(!empty($attr)){
+			if($type == 'textarea' && $nom == 'description' || $nom == 'content'){
+				$attributes = ['class' => 'form-control ckeditor', 'id' => $nom ,'placeholder' => $placeholder,'data-emojiable' => $attr];
+			}
+			else{
+				$attributes = ['class' => 'form-control', 'id' => $nom ,'placeholder' => $placeholder,'data-emojiable' => $attr];
+			}
+		}
+		else{
+			if($type == 'textarea' && $nom == 'description' || $nom == 'content'){
+				$attributes = ['class' => 'form-control ckeditor', 'id' => $nom ,'placeholder' => $placeholder];
+			}
+			else{
+				$attributes = ['class' => 'form-control', 'id' => $nom ,'placeholder' => $placeholder];
+			}
+		}
+		
+		return sprintf('
+			<div class="form-group %s %s">
+				%s
+				%s
+				%s
+				%s
+			</div>',
+			($colonnes == 0)? '': 'col-lg-' . $colonnes,
+			$errors->has($nom) ? 'has-error' : '',
+			$label ? $this->label($nom, $label, ['class' => 'control-label']) : '',
+			$pop? '<a href="#" tabindex="0" class="badge pull-right" data-toggle="popover" data-trigger="focus" title="' . $pop[0] .'" data-content="' . $pop[1] . '"><span>?</span></a>' : '',
+			call_user_func_array(['Form', $type], ($type == 'password')? [$nom, $attributes] : [$nom, $valeur, $attributes]),
+			$errors->first($nom, '<small class="help-block">:message</small>')
+		);
+	}
+	
 	public function control_new($type, $colonnes, $nom, $errors, $label = null, $valeur = null, $pop = null, $placeholder = '')
 	{
 		$attributes = ['class' => 'form-control', 'id' => $nom ,'placeholder' => $placeholder];
