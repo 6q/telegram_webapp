7b1a1800ad24dabc8a7aca5f92f76447f2eab3f8
6q <francesc@gestinet.com>
2017-03-03 12:37:50 +0100

    webservices amb variable

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -425,7 +425,7 @@ Route::post('/{bottoken}/webhook', function ($token) {
 					if ($bot_cmd_msg[0]->webservice_type == 1) {
 						$url=$bot_cmd_msg[0]->webservice_url;
 					} elseif ($bot_cmd_msg[0]->webservice_type == 2) {
-						$arr = explode(' ',trim($messageText));
+						$arr = preg_split('/ +/', $messageText);
 						if (array_key_exists(1,$arr)) {
 							$url = $bot_cmd_msg[0]->webservice_url.$arr[1];
 							$msg .= chr(10).$url;
