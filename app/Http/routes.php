4a963735fd527230838218218d4a884cecbfee61
6q <francesc@gestinet.com>
2017-03-03 11:24:47 +0100

    markdown

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -424,7 +424,7 @@ Route::post('/{bottoken}/webhook', function ($token) {
 					$xml = simplexml_load_file($bot_cmd_msg[0]->webservice_url);
 					//echo $xml->asXML();
 					if ($xml === false) {
-						echo "Failed loading XML: ";
+						$msg = "Failed loading XML: ";
 						foreach(libxml_get_errors() as $error) {
 							$msg .= chr(10). $error->message;
 						}
@@ -438,7 +438,7 @@ Route::post('/{bottoken}/webhook', function ($token) {
 							if ($i == 1){
 								foreach ($object as $resource) {
 
-									$msg .= chr(10). "<b>".$resource["name"]."</b>: ".$resource;
+									$msg .= chr(10). "*".$resource["name"]."*: ".$resource;
 								}
 							}
 							++$i;
@@ -456,7 +456,7 @@ Route::post('/{bottoken}/webhook', function ($token) {
         
         if($messageText != "\xE2\x97\x80"){
             DB::table('tmp_message')->insert(
-                ['chat_id' => $chatId,'message_id' => $message_id,'message' => $messageText.'_'.$dbBotId]
+                ['chat_id' => $chatId,'message_id' => $message_id,'message' => $messageText.'_'.$dbBotId,'parse_mode' => 'Markdown']
             );
         }
         
@@ -1157,7 +1157,8 @@ Route::post('/{bottoken}/webhook', function ($token) {
         $response = $telegram->sendMessage([
             'chat_id' => $chatId,
             'text' => $msg,
-            'reply_markup' => $reply_markup
+            'reply_markup' => $reply_markup,
+	        'parse_mode' => 'Markdown'
         ]);
     }
 
